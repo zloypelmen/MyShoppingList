210cb6335fcf46188b6e80b7d1c255f47f267a58
stack <magnum94@vk.com>
2024-12-26 00:35:59 +0300

    add realization repository to data layer

--- app/src/main/java/com/leonid/myshoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/leonid/myshoppinglist/domain/ShopItem.kt
@@ -1,8 +1,13 @@
 package com.leonid.myshoppinglist.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
     val enabled: Boolean,
-)
+    var id: Int = UNDEFINED_ID,
+){
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
+
