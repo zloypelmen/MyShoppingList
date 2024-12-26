0091523bbba76a42095de4a30a60214053570c3b
stack <magnum94@vk.com>
2024-12-27 01:21:07 +0300

    fix shopListAdapter

--- app/src/main/java/com/leonid/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/leonid/myshoppinglist/presentation/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.leonid.myshoppinglist.presentation
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.widget.Adapter
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.leonid.myshoppinglist.R
-import com.leonid.myshoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter: ShopListAdapter
+    private lateinit var shopListAdapter: ShopListAdapter
     private lateinit var viewModel: MainViewModel
     private val TAG = MainActivity::class.java.simpleName
 
@@ -29,17 +21,17 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            adapter.shopList = it
+            shopListAdapter.shopList = it
         }
     }
 
     private fun setupRecyclerView() {
         val rvShopList: RecyclerView = findViewById(R.id.rv_shop_list)
 
-        adapter = ShopListAdapter()
+        shopListAdapter = ShopListAdapter()
 
         with(rvShopList){
-            adapter = adapter
+            adapter = shopListAdapter
             recycledViewPool.setMaxRecycledViews(
                 ShopListAdapter.VIEW_TYPE_ENABLED,
                 ShopListAdapter.MAX_POOL_SIZE
