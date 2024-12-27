16a487bcf22e50e868d322a210bee56ed8ecb0a1
stack <magnum94@vk.com>
2024-12-27 23:11:02 +0300

    add ShopItemActivity and layout

---
+++ app/src/main/java/com/leonid/myshoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,16 @@
+package com.leonid.myshoppinglist.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.leonid.myshoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_shop_item)
+    }
+}
